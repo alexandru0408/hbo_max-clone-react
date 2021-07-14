fccb77dd971906961a2087515fe7942fb9c613b9
alexandru0408 <65963412+alexandru0408@users.noreply.github.com>
2021-07-14 20:44:24 +0300

    Added local fontawesome

--- pages/_document.js
+++ pages/_document.js
@@ -21,13 +21,7 @@ class MyDocument extends Document {
             as="style"
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Poppins:ital,wght@0,200;0,300;1,100;1,200;1,400&display=swap"
           />
-          <link
-            rel="preload"
-            as="style"
-            href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-            integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-            crossOrigin="anonymous"
-          />
+          <link rel="stylesheet" href="/fontawesome/css/all.min.css" />
         </Head>
         <body>
           <Main />
